1e2dd4d4b6189b8864a344ff4e8ade81cf2a567a
i0CT <211722662@qq.com>
2021-05-04 01:28:13 +0800

    初始化

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,13 +1,5 @@
 module.exports = app => {
   const mongoose = require('mongoose')
-  mongoose.connect('mongodb://127.0.0.1:27017/node-vue-moba', {
-    user: "root", pass: "123abc", authSource: "admin",
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }, (err) => {
-    if (err) {
-      console.log(err)
-    }
-  })
+  mongoose.connect('mongodb://127.0.0.1:27017/node-vue-moba')
   require('require-all')(__dirname + '/../models')
 }
