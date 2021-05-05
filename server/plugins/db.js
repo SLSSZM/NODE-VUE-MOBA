fbecc721935870897296a0f304400a4e4b553f60
i0CT <211722662@qq.com>
2021-05-05 15:47:48 +0800

    初始化

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,6 +1,6 @@
 module.exports = app => {
   const mongoose = require('mongoose')
-  mongoose.connect('mongodb:root@123abc//127.0.0.1:27017/node-vue-moba?authSource=admin', {
+  mongoose.connect('mongodb:root:123abc@//127.0.0.1:27017/node-vue-moba?authSource=admin', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
